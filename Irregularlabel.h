5b4a902b74a476cdb26e5cd9b2eab84f3dc3de52
wuxinyi <15091861910@163.com>
2018-06-03 18:34:47 +0800

    first

---
+++ Irregularlabel.h
@@ -0,0 +1,13 @@
+//
+//  Irregularlabel.h
+//  IrregularLabel
+//
+//  Created by wuxinyi on 18/6/3.
+//  Copyright © 2018年 wuxinyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Irregularlabel : UILabel
+
+@end
---
+++ Irregularlabel.m
@@ -0,0 +1,21 @@
+//
+//  Irregularlabel.m
+//  IrregularLabel
+//
+//  Created by wuxinyi on 18/6/3.
+//  Copyright © 2018年 wuxinyi. All rights reserved.
+//
+
+#import "Irregularlabel.h"
+
+@implementation Irregularlabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
